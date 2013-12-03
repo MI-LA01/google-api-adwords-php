1c368b1cf3ab7ffc5ab3f1c57cfed675c6dfb6c6
Alexis Gruet <agruet@Alexiss-MacBook-Pro.local>
2013-12-03 17:11:36 +0100

    Extracted BiddingStrategyService

---
+++ src/Google/Api/Ads/AdWords/v201306/Common/SharedBiddingStrategyBiddingStrategyStatus.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Google\Api\Ads\AdWords\v201306\Common;
+
+/**
+ * Status of the bidding strategy.
+ * @package GoogleApiAdsAdWords
+ * @subpackage v201306
+ */
+class SharedBiddingStrategyBiddingStrategyStatus {
+	/**
+	 * Gets the namesapce of this class
+	 * @return the namespace of this class
+	 */
+	public function getNamespace() {
+		return "https://adwords.google.com/api/adwords/cm/v201306";
+	}
+
+	/**
+	 * Gets the xsi:type name of this class
+	 * @return the xsi:type name of this class
+	 */
+	public function getXsiTypeName() {
+		return "SharedBiddingStrategy.BiddingStrategyStatus";
+	}
+
+	public function __construct() {
+	}
+}
